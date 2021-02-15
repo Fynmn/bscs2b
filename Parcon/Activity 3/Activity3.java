571d24106862e89e973d4532c6fe61e14de39595
Natalie Jane Pacificar <njanepacificar@wvsu.edu.ph>
2021-02-15 12:47:16 +0800

    Create Activity3.java

---
+++ Parcon/Activity 3/Activity3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Activity3 {
+
+    public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+
+        System.out.println("\tTemperature Converter\n\t(Celsius to Fahrenheit)\n");
+
+        System.out.print("Enter the Celsius value:");
+        double celsius = scan.nextDouble();
+
+        System.out.println("You have entered: " + celsius + "\u2103");
+
+        double result = (celsius * 9/5) + 32;
+
+        System.out.print("The converted temperature is " + result + "\u2109");
+    }
+}
